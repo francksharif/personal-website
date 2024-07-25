f117085307cf2de585e3e427d3c3d3930b8c7042
francksharif <badinisharif23@gmail.com>
2024-07-25 16:22:14 -0400

    making last changes for templates

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-
+from flask_ckeditor import CKEditor
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
+ckeditor = CKEditor()
 
 
 
@@ -18,6 +19,7 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
+    ckeditor.init_app(app)
     login_manager.login_view = 'auth.login'
 
     from .main import main as main_blueprint
